469a14a2fea0fecffc0afee5a912c584fea39ea3
Michael Pohoreski <Michael_Pohoreski@cable.comcast.com>
2016-07-11 10:06:17 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Code Clinic 2015: How to Write Code the Compiler Can Actually Optimize
+
+Mike Acton GDC:
+
+Code Clinic 2015: How to Write Code the Compiler Can Actually Optimize
+* http://www.gdcvault.com/play/1021866/Code-Clinic-2015-How-to
+
+Slides:
+* https://twitter.com/mike_acton/status/447254390916022272
+  * http://t.co/SL470y5LY9
+* http://www.slideshare.net/cellperformance/gdc15-code-clinic
+
+Includes:
+
+* A000081 Source Code
+
---
+++ test1.js
@@ -0,0 +1,54 @@
+// https://oeis.org/A000081
+// Based on:
+//   Comment from Joe Riel (joer(AT)san.rr.com), June 23, 2008:
+//   (Start) Here is a much more efficient method for computing the result with Maple
+//   a := proc(n) local k; a(n) := add(k*a*s(n-1, k), k=1..n-1)/(n-1) end proj:
+//   a(0) := 0: a(1) = 1: s := proc(n, k) local j; s(n,k) := add(a(n+1-j*k), j=1..iqou(n, k)); (End)
+function A000081( len ) {
+
+    function s( n, k ) {
+        var end = Math.floor( n/k );
+        var sum = 0;
+        for( var j = 1; j <= end; j++ )
+            sum += a( n+1 - j*k );
+        
+        return sum;
+    }
+
+
+    function a( n ) {
+        process.stdout.write( n + ' ' );
+        if( n === 0 ) 
+            return 0;
+
+        if( n === 1 ) 
+            return 1;
+
+
+        var sum = 0;
+        for( var k = 1; k <= n-1; k++ ) 
+            sum += k * a(k) * s( n-1, k );
+        
+        return sum / (n-1);
+    }
+
+    var result = [];
+    for( var i = 0; i < len; i++ ) {
+        result[i] = a(i);
+        process.stdout.write( '\n' ); // <- moved #next
+    }
+
+    return result;
+}
+
+var start  = process.hrtime();
+var result = A000081( 20 );
+var end    = process.hrtime(start);
+
+console.log( result );
+
+var time_sec = end[0];
+var time_ms  = end[1] / 1000000;
+console.log( time_sec + " s, " + time_ms.toFixed(3) + " ms" );
+
+
