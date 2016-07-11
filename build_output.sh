029b0998f2f6b5a78c6af938b6605cdad9339dc8
Michael Pohoreski <Michael_Pohoreski@cable.comcast.com>
2016-07-11 11:03:57 -0700

    Added test 5, cleanup output

---
+++ build_output.sh
@@ -0,0 +1,7 @@
+echo "0..."; node test0.js > output0.txt
+echo "1..."; node test1.js > output1.txt
+echo "2..."; node test2.js > output2.txt
+echo "3..."; node test3.js > output3.txt
+echo "4..."; node test4.js > output4.txt
+echo "5..."; node test5.js > output5.txt
+
